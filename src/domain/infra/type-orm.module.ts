6d91b9bd30a9ffca7ff4c3f792e4697229202f68
DevDeyvidTav <devdeyvid2022@gmail.com>
2025-01-10 00:21:09 -0300

    feat: connect database

---
+++ src/domain/infra/type-orm.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: configService.get<boolean>('DATABASE_SYNCRONIZE'),
+      }),
+    }),
+  ],
+})
+export class InfraModule {}
