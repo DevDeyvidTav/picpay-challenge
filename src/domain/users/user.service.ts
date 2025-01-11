abe9b92ba6758a9582b5aba40a4eb15cd6c75a83
DevDeyvidTav <devdeyvid2022@gmail.com>
2025-01-11 12:23:36 -0300

    refactor: user service to promisse.all in create

--- src/domain/users/user.service.ts
+++ src/domain/users/user.service.ts
@@ -1,70 +1,73 @@
-import {
-  BadRequestException,
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
-import { AbstractUserRepository } from './user-repository.abstract';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, Inject } from "@nestjs/common";
+import { AbstractUserRepository } from "./user-repository.abstract";
+import { CreateUserDto } from "./dtos/create-user.dto";
 
 @Injectable()
 export class UserService {
-  constructor(
-    @Inject('UseAbstractUserRepository') // Certifique-se de usar o mesmo token
-    private readonly repository: AbstractUserRepository,
-  ) {}
+    constructor(
+        @Inject('UseAbstractUserRepository')
+        private readonly repository: AbstractUserRepository,
+    ) {}
 
-  async create(user: CreateUserDto) {
-    try {
-      const alreadyExistsThisEmail = await this.repository.findByEmail(
-        user.email,
-      );
-      const alreadyExistsThisCpf = await this.repository.findByCpf(user.cpf);
+    async create(user: CreateUserDto) {
+        try {
+            const [emailExists, cpfExists] = await Promise.all([
+                this.repository.findByEmail(user.email),
+                this.repository.findByCpf(user.cpf),
+            ]);
 
-      if (alreadyExistsThisEmail) {
-        throw new BadRequestException('Já existe um usuário com esse email');
-      }
+            if (emailExists) {
+                throw new BadRequestException('Já existe um usuário com esse email');
+            }
 
-      if (alreadyExistsThisCpf) {
-        throw new BadRequestException('Já existe um usuário com esse cpf');
-      }
+            if (cpfExists) {
+                throw new BadRequestException('Já existe um usuário com esse cpf');
+            }
 
-      return await this.repository.create(user);
-    } catch (error) {
-      console.error(error);
-      throw new InternalServerErrorException('Algo deu errado!');
+            return await this.repository.create(user);
+        } catch (error) {
+            console.error('Erro ao criar usuário:', error);
+            throw new InternalServerErrorException('Erro ao criar usuário');
+        }
     }
-  }
 
-  async findAll() {
-    try {
-      return await this.repository.findAll();
-    } catch (error) {
-      throw error;
+    async findAll() {
+        try {
+            return await this.repository.findAll();
+        } catch (error) {
+            console.error('Erro ao buscar todos os usuários:', error);
+            throw new InternalServerErrorException('Erro ao buscar usuários');
+        }
     }
-  }
 
-  async findById(id: string) {
-    try {
-      return await this.repository.findById(id);
-    } catch (error) {
-      throw error;
+    async findById(id: string) {
+        try {
+            const user = await this.repository.findById(id);
+            if (!user) {
+                throw new BadRequestException('Usuário não encontrado');
+            }
+            return user;
+        } catch (error) {
+            console.error(`Erro ao buscar usuário com ID ${id}:`, error);
+            throw new InternalServerErrorException('Erro ao buscar usuário');
+        }
     }
-  }
 
-  async findByEmail(email: string) {
-    try {
-      return await this.repository.findByEmail(email);
-    } catch (error) {
-      throw error;
+    async findByEmail(email: string) {
+        try {
+            return await this.repository.findByEmail(email);
+        } catch (error) {
+            console.error(`Erro ao buscar usuário com email ${email}:`, error);
+            throw new InternalServerErrorException('Erro ao buscar usuário por email');
+        }
     }
-  }
 
-  async findByCpf(cpf: string) {
-    try {
-      return await this.repository.findByCpf(cpf);
-    } catch (error) {
-      throw error;
+    async findByCpf(cpf: string) {
+        try {
+            return await this.repository.findByCpf(cpf);
+        } catch (error) {
+            console.error(`Erro ao buscar usuário com CPF ${cpf}:`, error);
+            throw new InternalServerErrorException('Erro ao buscar usuário por CPF');
+        }
     }
-  }
 }
